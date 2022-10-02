447f0619c62d79ab306493c75466935a83de2d2d
Ryan Ardito <ryan.m.ardito@gmail.com>
2022-10-02 17:19:48 -0600

    added tests

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -7,17 +7,20 @@
  */
 
 use std::collections::{HashMap, HashSet};
-use std::time::{SystemTime, Duration};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, near_bindgen, require, AccountId, Promise};
+use near_sdk::{env, log, near_bindgen, require, AccountId, ONE_NEAR};
 
 // Define the default message
 const STARTING_POSITION: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 const VOTING_PERIOD_INTERVAL: u64 = 30;
 
-enum EndState {
+enum GameState {
+    ACTIVE,
+    DRAWOFFER,
     WHITEWIN,
     BLACKWIN,
+    WHITERESIGN,
+    BLACKRESIGN,
     STALEMATE,
     DRAW,
 }
@@ -27,7 +30,7 @@ enum EndState {
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     game_active: bool,
-    fen_state: String,
+    board_fen: String,
     buyin_amount: u128,
     next_period_timestamp: u64,
     votes: HashMap<String, u64>,
@@ -41,8 +44,8 @@ impl Default for Contract{
     fn default() -> Self{
         Self {
             game_active: true,
-            fen_state: STARTING_POSITION.to_string(),
-            buyin_amount: 1_000_000_000_000_000_000_000_000,  // 1 near in yoctonear
+            board_fen: STARTING_POSITION.to_string(),
+            buyin_amount: ONE_NEAR,
             next_period_timestamp: env::block_timestamp() + VOTING_PERIOD_INTERVAL,
             votes: HashMap::new(),
             voted_this_period: HashSet::new(),
@@ -55,16 +58,19 @@ impl Default for Contract{
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
+    pub fn new(buyin_amount: u128) -> Self {
+        Self { buyin_amount, ..Default::default() }
+    }
     pub fn get_fen(self) -> String {
-        self.fen_state
+        self.board_fen
     }
 
     #[payable]
     pub fn add_player(&mut self, player_address: AccountId) {
-        // verify that game still in buy-in period
-        // require!(self.fen_state.split(" ").last() == Some("1"), "buy-in period is over");
+        // // verify that game still in buy-in period
+        // require!(self.board_fen.split(" ").last() == Some("1"), "buy-in period is over");
         // transfer buy-in to contract
-        //require!(env::attached_deposit() > self.buyin_amount, "send more coins lol");  // using payable function
+        require!(env::attached_deposit() >= self.buyin_amount, "send more coin lol");  // using payable function
         // add player to random color
         if self.white_players.is_empty() {
             self.white_players.insert(player_address);
@@ -83,7 +89,7 @@ impl Contract {
     pub fn cast_vote(&mut self, board_fen: String, vote_fen: String) {
         let player_address = env::signer_account_id();
         // verify voter has correct board state
-        require!(board_fen == self.fen_state, "out of date board state");
+        require!(board_fen == self.board_fen, "out of date board state");
         // verify player has voting rights
         require!(self.voting_rights(&player_address), "player can't vote in this game");
         // verify player hasn't voted this period
@@ -92,6 +98,8 @@ impl Contract {
         require!(self.can_vote(&player_address), "it's not your turn");
         // add vote to votes
         *self.votes.entry(vote_fen).or_insert(0) += 1;
+        // add player to voted this period
+        self.voted_this_period.insert(player_address);
     }
 
     pub fn voting_rights(&self, player_address: &AccountId) -> bool {
@@ -99,12 +107,12 @@ impl Contract {
     }
 
     pub fn can_vote(&self, player_address: &AccountId) -> bool {
-        let players_to_move = match self.fen_state.split(" ").nth(1) {
+        let players_to_move = match self.board_fen.split(" ").nth(1) {
             Some("w") => &self.white_players,
             Some("b") => &self.black_players,
             _ => env::panic_str("malformed FEN"),
         };
-        players_to_move.contains(player_address)
+        players_to_move.contains(player_address) || !self.voted_this_period.contains(player_address)
     }
 
     // verify next vote timestamp is in the past, select winnning vote and update state
@@ -113,7 +121,7 @@ impl Contract {
         require!(env::block_timestamp() > self.next_period_timestamp, "current voting period not over");
         // find highest voted fen
         let mut curr_most_votes = 0;
-        let mut winning_fen = &self.fen_state;
+        let mut winning_fen = &self.board_fen;
         for (key, val) in &self.votes {
             if *val > curr_most_votes {
                 curr_most_votes = *val;
@@ -127,7 +135,7 @@ impl Contract {
         //     Some("other stuff") => { todo!(); },
         //     _ => (),
         // };
-        self.fen_state = winning_fen.to_owned();
+        self.board_fen = winning_fen.to_owned();
         // empty votes map
         self.votes.clear();
         // empty voted_this_period set
@@ -159,31 +167,41 @@ impl Contract {
     // }
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
+#[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
-    // use super::*;
+    use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::{testing_env, VMContext, Promise};
 
-    // #[test]
-    // fn get_default_greeting() {
-    //     let contract = Contract::default();
-    //     // this test did not call set_greeting so should return the default "Hello" greeting
-    //     assert_eq!(
-    //         contract.get_greeting(),
-    //         "Hello".to_string()
-    //     );
-    // }
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("bob_near".parse().unwrap())
+            .is_view(is_view)
+            .build()
+    }
 
-    // #[test]
-    // fn set_then_get_greeting() {
-    //     let mut contract = Contract::default();
-    //     contract.set_greeting("howdy".to_string());
-    //     assert_eq!(
-    //         contract.get_greeting(),
-    //         "howdy".to_string()
-    //     );
-    // }
+    #[test]
+    fn board_fen_should_be_starting_fen() {
+        let context = get_context(true);
+        testing_env!(context);
+        // ... Write test here
+        let buyin = ONE_NEAR;
+        let contract = Contract::new(buyin);
+        assert_eq!(
+            contract.get_fen(),
+            STARTING_POSITION.to_string(),
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "send more coin")]
+    fn bob_cannot_buyin_without_deposit() {
+        let context = get_context(false);
+        testing_env!(context);
+        // ... Write test here
+        let bob: AccountId = "bob_near".parse().unwrap();
+        let mut contract = Contract::new(ONE_NEAR);
+        
+        contract.add_player(bob.clone());
+    }
 }
